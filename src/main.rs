255602de119ea5892f05844ab29c397097019545
Brandon <Brandon.wesley@ntlworld.com>
2026-08-13 15:01:14 +0100

    Add thread-safe store with Arc<Mutex<HashMap>>>, test insert/print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
 fn main() {
-    println!("Hello, world!");
+    // (HashMap::new) make an empty map, (Mutex::new) wrap it for safe access, (Arc::new) wraps that so it can be shared
+    let store: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(HashMap::new()));
+
+    {
+        let mut map = store.lock().unwrap(); // unwrap() because .lock() can fail 
+        map.insert("hello".to_string(), "world".to_string());
+    } // lock is released automatically
+
+    {
+        let map = store.lock().unwrap();
+        println!("{:?}", map);
+    }
 }
